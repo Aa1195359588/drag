cab77e4373c581486868bfa8a5eb9c23cc3a0bf3
wenjinhong <wjh901029@foxmail.com>
2017-04-05 22:02:34 +0800

    change1 drag.js

--- drag.js
+++ drag.js
@@ -2,5 +2,9 @@ function drag(id){
   var obj = document.getElementById(id);
   var disx = 0;
   var disy = 0;
-  obj.onmousedown = function(){};
+  obj.onmousedown = function(ev){
+    ev = ev||event;
+    disx = ev.clientX - obj.offsetLeft;
+    disy = ev.clientY - obj.offsetTop;
+  };
 };
